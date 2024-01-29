f563bff0557ee09e87e41ff4f1c1cdb324d9d4e3
mutaqinmi <mutaqinmi2586@gmail.com>
2024-01-29 18:11:48 +0700

    Files

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const fastify = require('fastify')();
+const fs = require('fs');
+const util = require('util');
+const {pipeline} = require('node:stream')
+const pump = util.promisify(pipeline);
 const path = require('path');
 const time = require('moment')();
 
@@ -7,6 +11,8 @@ fastify.register(require('@fastify/view'), {
         ejs: require('ejs'),
     },
 });
+
+fastify.register(require('@fastify/multipart'))
 
 fastify.register(require('@fastify/static'), {
     root: path.join(__dirname, 'public'),
@@ -20,6 +26,14 @@ fastify.get('/peminjaman-laptop', (req, res) => {
     res.view("/views/peminjaman-laptop.ejs", {
         time: time.format("YYYY-MM-DDTHH:mm"),
     })
+})
+
+fastify.post("/peminjaman-laptop", (req, res) => {
+    const files = req.files();
+
+    console.log(files.filename);
+    // pump(files.file, fs.createWriteStream(`public/uploads/${files.filename}`))
+    // res.send("Ok")
 })
 
 fastify.listen({port:8000}, () => {
