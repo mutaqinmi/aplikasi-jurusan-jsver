f411410320499192a19ca285b09379387498acc8
mutaqinmi <mutaqinmi2586@gmail.com>
2024-01-29 15:30:14 +0700

    Optimize peminjaman

--- public/script/camera.js
+++
@@ -1,7 +0,0 @@
-const show = (e) => {
-    if(e.target.files.length > 0){
-        var src = URL.createObjectURL(e.target.files[0]);
-        var preview = document.getElementById("image");
-        preview.src = src;
-    }
-}
---
+++ public/script/peminjaman-laptop.js
@@ -0,0 +1,18 @@
+const show = (e) => {
+    if(e.target.files.length > 0){
+        var src = URL.createObjectURL(e.target.files[0]);
+        var preview = document.getElementById("result-image");
+        preview.src = src;
+        preview.style.height = "5rem";
+        
+        const submitButton = document.getElementsByClassName("submit")[0];
+        if(submitButton.hasChildNodes() === false){
+            const button = document.createElement("button");
+            button.innerHTML = "<i class='ph ph-check'></i>";
+            button.setAttribute("type", "submit");
+            button.setAttribute("class", "submit-button");
+    
+            submitButton.appendChild(button)
+        }
+    }
+}
