eb8e8bbb71ae55881f41fb243f311d827cec4dbf
Raffi14 <raffifabiansyah@gmail.com>
2024-01-29 11:41:47 +0700

    kamera

---
+++ public/script/camera.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("camera");
+
+    function Show(event) {
+        if(event.target.files.length > 0){
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("image");
+            preview.src = src;
+          }
+    }
+
