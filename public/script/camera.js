c69e2ac45214bff19f8ec3d20ca38953e145aaa2
mutaqinmi <mutaqinmi2586@gmail.com>
2024-01-29 13:04:00 +0700

    Update camera

--- public/script/camera.js
+++ public/script/camera.js
@@ -1,10 +1,11 @@
+const show = (e) => {
+    if(e.target.files.length > 0){
+        var src = URL.createObjectURL(e.target.files[0]);
+        var preview = $("#image");
+        preview.src = src;
+    }
+}
+
 $(document).ready(() => {
-    $("#camera").change((e) => {
-        alert("Ok")
-        if(e.target.files.length > 0){
-            var src = URL.createObjectURL(e.target.files[0]);
-            var preview = $("#image");
-            preview.src = src;
-        }
-    })
+    $("#camera").on("change", show(event))
 })
